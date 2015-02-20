a779faf0e700254e0e32c67b4c05b9c4b8c26418
JamieSinn <james.sinn@sinndevelopment.com>
2015-02-20 16:08:38 -0500

    Overhaul to current state.

--- EcoCityCraftVoter/Assets/Scripts/EcoCityCraftVoter.cs
+++ EcoCityCraftVoter/Assets/Scripts/EcoCityCraftVoter.cs
@@ -9,8 +9,9 @@ public class EcoCityCraftVoter : MonoBehaviour
     private static float Width = Screen.width;
     private static float Height = Screen.height / 8;
     private static float ButtonStart = Screen.height / 6;
+    public Texture Background;
 
-
+    public static Rect BG = new Rect(0, 0, Width, Screen.height);
     public static Rect Forums = new Rect(0, ButtonStart +  Height, Width, Height);
     public static Rect V1 = new Rect(0, ButtonStart + 2 * Height + 5, Width, Height);
     public static Rect V2 = new Rect(0, ButtonStart + 3 * Height + 5, Width, Height);
@@ -19,25 +20,34 @@ public class EcoCityCraftVoter : MonoBehaviour
 
     void OnGUI()
     {
-        if (GUI.Button(Forums, "EcoCityCraft Forums"))
+        GUI.contentColor = Color.white;
+        GUI.backgroundColor = Color.white;
+        GUIStyle myStyle = new GUIStyle();
+        myStyle.fontSize = 50;
+        
+        GUI.DrawTexture(BG, Background);
+    
+        
+        if (GUI.Button(Forums, "EcoCityCraft Forums", myStyle))
         {
             Application.OpenURL("http://ecocitycraft.com");
         }
-        if (GUI.Button(V1, "Vote Site 1"))
+        if (GUI.Button(V1, "Vote Site 1", myStyle))
         {
             Application.OpenURL(VoteSites.Vote1);
         }
-        if (GUI.Button(V2, "Vote Site 2"))
+        if (GUI.Button(V2, "Vote Site 2", myStyle))
         {
             Application.OpenURL(VoteSites.Vote2);
         }
-        if (GUI.Button(V3, "Vote Site 3"))
+        if (GUI.Button(V3, "Vote Site 3", myStyle))
         {
             Application.OpenURL(VoteSites.Vote3);
         }
-        if (GUI.Button(SinnDev, "Sinn Development"))
+        if (GUI.Button(SinnDev, "Sinn Development", myStyle))
         {
             Application.OpenURL("http://sinndevelopment.com");
         }
+        
     }
 }
